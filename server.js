cf335c7f306529a642cecdbfbb516f6574c7dc8c
grigjorwebogdan8-ctrl <grigjorwebogdan8@gmail.com>
2026-04-17 20:14:57 +0100

    add websocket server

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const WebSocket = require("ws");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("WebSocket server is running");
+});
+
+const wss = new WebSocket.Server({ noServer: true });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.send("Hello from WS");
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (req.url === "/ws") {
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      wss.emit("connection", ws, req);
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("WS server started on port " + PORT);
+});
