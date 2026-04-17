cefa3cc80c5d4850c58007780f3a2200835e0554
grigjorwebogdan8-ctrl <grigjorwebogdan8@gmail.com>
2026-04-17 21:23:35 +0100

    fix ws server

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-const http = require("http");
 const WebSocket = require("ws");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("WebSocket server is running");
-});
+const PORT = process.env.PORT || 10000;
 
-const wss = new WebSocket.Server({ noServer: true });
+const wss = new WebSocket.Server({ port: PORT });
 
 wss.on("connection", (ws) => {
   console.log("Client connected");
-  ws.send("Hello from WS");
 
   ws.on("message", (msg) => {
     const data = JSON.parse(msg);
 
-    // === Обработка ставки ===
     if (data.type === "bet") {
       console.log("New bet:", data);
 
-      // Рассылка всем игрокам
       wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(JSON.stringify({
@@ -33,17 +26,4 @@ wss.on("connection", (ws) => {
   });
 });
 
-server.on("upgrade", (req, socket, head) => {
-  if (req.url === "/ws") {
-    wss.handleUpgrade(req, socket, head, (ws) => {
-      wss.emit("connection", ws, req);
-    });
-  } else {
-    socket.destroy();
-  }
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, "0.0.0.0", () => {
-  console.log("WS server started on port " + PORT);
-});
+console.log("WS server started on port " + PORT);
